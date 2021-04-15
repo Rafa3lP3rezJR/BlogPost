1461d8f7e1419e9b06d8f7dd9736aff28dff793e
Rafael Pérez <68167167+Rafa3lP3rez@users.noreply.github.com>
2021-04-14 22:26:26 -0600

    Esta es la version final del proyecto

--- index.php
+++ index.php
@@ -12,6 +12,7 @@
 
     <h1>Esta es la cabecera del proyecto</h1>
 
+	<h2>Esta es la versión 3 de nuestro proyecto</h2>
 
     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam, aliquid. Ex iste, hic quia rem ipsam sequi ad.
         Dolore deleniti voluptatibus nihil recusandae animi architecto ipsum rem repellat magni id!</p>
