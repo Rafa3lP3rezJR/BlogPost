d3fd689d17eabade1c88f2a5a59cc21a969b4f13
Rafael Pérez <68167167+Rafa3lP3rez@users.noreply.github.com>
2021-04-14 21:54:03 -0600

    Estructura de HTML lista

--- index.php
+++ index.php
@@ -1,8 +1,29 @@
-<?php
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proyecto</title>
+</head>
+
+<body>
+
+    <h1>Esta es la cabecera del proyecto</h1>
 
 
-	echo "Hello World";
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Numquam, aliquid. Ex iste, hic quia rem ipsam sequi ad.
+        Dolore deleniti voluptatibus nihil recusandae animi architecto ipsum rem repellat magni id!</p>
+    <?php
 
 
+			echo "Hello World";
 
-?>
+			
+
+	?>
+
+</body>
+
+</html>
